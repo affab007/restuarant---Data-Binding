7402e9cdb7da1cbe153ba55a1bf0d90e7773add3
affab007 <affab007@gmail.com>
2022-02-28 20:24:00 +0700

    Edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodMenuItem } from './food-menu-item/food menu item';
+import { FoodMenuComponent } from './food-menu/food-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    FoodMenuItem,
+    AppComponent,
+    FoodMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
